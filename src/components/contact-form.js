f99da9db2d47e77c9939a03f81d5f4f6b03f5eb7
Danillo Lucas Nogueira <entelgy.danilo.nogueira@ext.gympass.com>
2021-02-19 10:11:39 -0300

    initial commit

---
+++ src/components/contact-form.js
@@ -0,0 +1,61 @@
+import React, { useState, useContext } from 'react';
+import { Segment, Form, Input, Button } from 'semantic-ui-react';
+import _ from 'lodash';
+import { ContactContext } from './../contexts/contact-context';
+
+function useFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = (e) => {
+    return setValue(e.target.value);
+  }
+
+  const handleReset = () => {
+    setValue('');
+  };
+
+  return {
+    value,
+    onChange: handleChange,
+    onReset: handleReset
+  };
+}
+
+export default function ContactForm() {
+  const name = useFormInput('');
+  const email = useFormInput('');
+  const [state, dispatch] = useContext(ContactContext);
+
+  const onSubmit = () => {
+    dispatch({
+      type: 'ADD_CONTACT',
+      payload: { 
+        id: _.uniqueId(10), 
+        name: name.value,
+        email: email.value
+      }
+    });
+    name.onReset();
+    email.onReset();
+  };
+
+  return (
+    <Segment basic>
+      <Form onSubmit={onSubmit}>
+        <Form.Group widths="3">
+          <Form.Field width={6}>
+            <Input placeholder="Enter Name" {...name} required />
+          </Form.Field>
+          <Form.Field width={6}>
+            <Input placeholder="Enter Email" {...email} type="email" required />
+          </Form.Field>
+          <Form.Field width={4}>
+            <Button fluid primary>
+              New Contact
+            </Button>
+          </Form.Field>
+        </Form.Group>
+      </Form>
+    </Segment>
+  );
+}
